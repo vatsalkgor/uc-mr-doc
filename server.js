4351f6964fb4e3c567b4f5ed2d5aca425d0f68da
Vatsal <vatsalkgor@gmail.com>
2019-09-25 11:38:23 +1200

    initial req

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 app.set('view engine','pug');
 
-
+app.get('/',(req,res)=>{
+    console.log("hello from express");
+    res.send("hello from heroku");
+})
 
 app.listen(3000),()=>{
 console.log("started on 3000")
