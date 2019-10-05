b448529c1f2f0c96c8ecbf5f97d7530b1cfbf175
Vatsal <vatsalkgor@gmail.com>
2019-10-06 11:42:51 +1300

    event emitter added for insert knowabout

--- server/index.js
+++ server/index.js
@@ -5,9 +5,14 @@ const jp = bp.json();
 const port = process.env.PORT || 8080;
 const connection = require('./db');
 const data = require('./routes/data');
+const events = new require("events");
+const emitter = new events.EventEmitter();
+emitter.on('insert_knowabout',()=>{
+    location.reload();
+})
 app.set('view engine', 'pug');
-app.set('json spaces',40);
-
+app.set('json spaces', 40);
+app.use(jp);
 app.get('/', (req, res) => {
     // console.log("hello from express");
     res.render("index");
@@ -16,34 +21,33 @@ app.get('/', (req, res) => {
 let options = {
     dotfiles: 'ignore',
     etag: false,
-    extensions: ['pug','js','css'],
+    extensions: ['pug', 'js', 'css'],
     index: false,
     maxAge: '1d',
     redirect: false,
     setHeaders: function (res, path, stat) {
-      res.set('x-timestamp', Date.now())
+        res.set('x-timestamp', Date.now())
     }
-  }
-  
-  app.use(express.static('views', options))
-  
+}
+
+app.use(express.static('views', options))
+
 // req.on('data', (data, err) => {
-app.post('/webhook', jp, (req, res) => {
+app.post('/webhook', (req, res) => {
     if (req.body.queryResult.intent.displayName.toLowerCase() == "knowabout") {
         session_array = req.body.session.split('/');
         connection.execute(`insert into uc_data(session_id,intent) VALUES('${session_array[session_array.length - 1]}','${req.body.queryResult.intent.displayName}')`, (err, results, fields) => {
             if (!err) {
-                console.log(results);
-                console.log(fields);
-                res.json({
-                    "fulfillmentText":req.body.queryResult.fulfillmentText,
-                    "payload":{
-                        "google":{
-                            "expectUserResponse":true,
-                            "richResponse":[
+                emitter.emit('insert_knowabout');
+                return res.json({
+                    "fulfillmentText": req.body.queryResult.fulfillmentText,
+                    "payload": {
+                        "google": {
+                            "expectUserResponse": true,
+                            "richResponse": [
                                 {
-                                    "simpleResponse":{
-                                        "textToSpeech":req.body.queryResult.fulfillmentText
+                                    "simpleResponse": {
+                                        "textToSpeech": req.body.queryResult.fulfillmentText
                                     }
                                 }
                             ]
@@ -55,7 +59,7 @@ app.post('/webhook', jp, (req, res) => {
     }
 })
 
-app.use('/api/data',data);
+app.use('/api/data', data);
 
 app.listen(port), () => {
     console.log(`started server on port{PORT}`);
