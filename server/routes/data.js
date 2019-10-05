f85add05ed87cb668c3d2a4e08b79b1aea8620dc
Vatsal <vatsalkgor@gmail.com>
2019-10-06 09:10:59 +1300

    node_modules removed and knowabout working

--- server/routes/data.js
+++ server/routes/data.js
@@ -2,10 +2,19 @@ const data = require("express").Router();
 const knowabout = require('../models/knowabout')
 
 data.get('/knowabout',async (req,res,next)=>{
-    // let data = await knowabout.getKnowAbouts();
-    // console.log(data) // returning undefined
-    res.json(await knowabout.getKnowAbouts()); // prints data to console. does not send to the response
-    // res.send('hi')
+    const resp = await knowabout.getKnowAbouts();
+    console.log(typeof(resp))
+    let data=[]
+    let lable=[];
+    Object.keys(resp).forEach(key=>{
+        data.push(resp[key].ic)
+        lable.push(new Date(resp[key].added));
+    })
+    // resp.foreach(r=>{
+    //     data.push(r.ic);
+    //     lable.push(new Date(r.added))
+    // })
+    res.json({data,lable});
 })
 
 module.exports = data;
--- views/js/chart.js
+++ views/js/chart.js
@@ -1,7 +1,28 @@
-$.ajax({
-    method:"GET",
-    url:"/api/data/knowabout",
-    success:function (data){
-        data = JSON.parse(data);
-    }
-})
+fetch('/api/data/knowabout')
+    .then(resp => resp.json())
+    .then((result) => {
+        console.log(result.data)
+        let c_c = {
+
+        }
+        let knowaboutChart = new Chart($("#knowabout"), {
+            type: 'line',
+            data: {
+                labels: result.label,
+                datasets: [{
+                    label: "KnowAbout Requests",
+                    data: result.data,
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'purple',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'purple',
+                    pointBackgroundColor: 'grey',
+                    pointRadius: 5,
+                    pointHoverRadius: 7,
+                    pointHitRadius: 15,
+                    pointBorderWidth: 2
+                }]
+            }
+        })
+    })
