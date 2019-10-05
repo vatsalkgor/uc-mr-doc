cf9af82f9d8d8803c5055f799c09e3984efcd5cb
Vatsal <vatsalkgor@gmail.com>
2019-10-05 15:10:13 +1300

    database changed

--- server/db.js
+++ server/db.js
@@ -1,10 +1,18 @@
 const mysql = require("mysql2");
 
+// const connection = mysql.createConnection({
+//     host:'sql113.epizy.com',
+//     user:'epiz_23055073',
+//     password:'Syf8FYiZ9aG',
+//     database:'epiz_23055073_uc'
+// });
+
 const connection = mysql.createConnection({
-    host:'sql112.epizy.com',
-    user:'epiz_23055073',
-    password:'Syf8FYiZ9aG',
-    database:'epiz_23055073_uc'
-});
+    host:'46.17.175.22',
+    user:'u989048914_uc',
+    password:'vatsalg007',
+    database:'u989048914_uc'
+})
+
 
 module.exports = connection;
